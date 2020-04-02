e7b0f7e3ff4563c88d6f729e1f8da16d3870098b
sjdrabbani <sajid@esenteq.com>
2020-04-02 09:01:26 +0600

    Implement Authentication middleware with DB initializer - Implement jwt authentication middleware - Implement postgres db initializer

---
+++ app/auth.go
@@ -0,0 +1,79 @@
+package app
+
+import (
+	"net/http"
+	u "github.com/gocontactmanager/utils"
+	"strings"
+	"github.com/gocontactmanager/models"
+	jwt "github.com/dgrijalva/jwt-go"
+	"os"
+	"context"
+	"fmt"
+)
+
+var JwtAuthentication = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		notAuth := [string]{"/api/user/new", "/api/user/login"}
+		requestPath := r.URL.Path
+
+		// Check whether the page needs authentication
+		for _, value := range notAuth {
+			if value == requestPath {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		response := make(map[string] interface{})
+		tokenHeader := r.Header.Get("Authorization")
+
+		// Token is missing, return 403
+		if tokenHeader == "" {
+			response = u.Message(false, "Missing auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		splitted := strings.Split(tokenHeader, " ") // Segregating the token from Bearer
+		if len(splitted) != 2 {
+			response = u.Message(false, "Invalid/Malformed auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		tokenPart := splitted[1] // Grab the actual token
+		tk := &models.Token()
+
+		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+
+		if err != nil { //Malformed token, returns with http code 403 as usual
+			response = u.Message(false, "Malformed authentication token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		if !token.Valid { //Token is invalid, maybe not signed on this server
+			response = u.Message(false, "Token is not valid.")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		//Everything went well, proceed with the request and set the caller to the user retrieved from the parsed token
+		fmt.Sprintf("User %", tk.Username) //Useful for monitoring
+		ctx := context.WithValue(r.Context(), "user", tk.UserId)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r) //proceed in the middleware chain!
+
+	})
+}
